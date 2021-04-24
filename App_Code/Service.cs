5e79fe67e9fcb8af32bdba4abd81c6eb80257ade
Reharsh <reharshdeshpande@gmail.com>
2021-04-24 10:16:34 +0530

    Changes for Stored Procedure

--- App_Code/Service.cs
+++ App_Code/Service.cs
@@ -27,44 +27,46 @@ public class Service : System.Web.Services.WebService
         return "Hello World";
     }
     [WebMethod]
-    public int Fill_Form(EnqueryFields ef)
+    public string Fill_Form(EnqueryFields ef)
     {
 
         EnqueryFormLogin DB = new EnqueryFormLogin();
-        int txt = DB.Fill_Enquery_form(ef);
-        return txt;
+         
+        return DB.Fill_Enquery_form(ef);
     }
     [WebMethod]
-    public int Fill_Coun_Form(CounsellingFields ef)
+    public string Fill_Coun_Form(CounsellingFields ef)
     {
 
         CounsellingFormLogic DB = new CounsellingFormLogic();
-        int txt = DB.Fill_Counselling_Form(ef);
-        return txt;
+       
+        return  DB.Fill_Counselling_Form(ef);
     }
 
     [WebMethod]
-    public int Stud_Login(StudLoginFields ef)
+    public string Stud_Login(StudLoginFields ef)
     {
 
         StudLoginLogic DB = new StudLoginLogic();
-        int txt = DB.login(ef);
+        string txt = DB.login(ef);
         return txt;
     }
     [WebMethod]
-    public int Genrate_Pass(StudLoginFields ef)
+    public string Genrate_Pass(StudLoginFields ef)
     {
 
         GenratePassLogic DB = new GenratePassLogic();
-        int txt = DB.Gen_Pass(ef);
-        return txt;
+        
+        string r= DB.Gen_Pass(ef);
+
+        return r;
     }
     [WebMethod]
-    public int Ask_Again_Que(EnqueryFields ef)
+    public string Ask_Again_Que(EnqueryFields ef)
     {
 
         AskQueryAfterLogin DB = new AskQueryAfterLogin();
-        int txt = DB.Ask_Que_Again(ef);
+        string txt = DB.Ask_Que_Again(ef);
         return txt;
     }
     [WebMethod]
@@ -83,19 +85,19 @@ public class Service : System.Web.Services.WebService
         return al.Pre_Fill(ef);
     }
     [WebMethod]
-    public int ARF_Fill(ARFFormFields ef)
+    public string ARF_Fill(ARFFormFields ef)
     {
 
         ARFFillingLogic al = new ARFFillingLogic();
-        al.ARF_Fill_Logic(ef);
-        return 0;
+        
+        return al.ARF_Fill_Logic(ef);
     }
     [WebMethod]
-    public int PastEdu_Details(PastEduFields ef)
+    public string PastEdu_Details(PastEduFields ef)
     {
 
         PastEduSaveLogic al = new PastEduSaveLogic();
-        int x = al.Save_Past(ef);
+        string x = al.Save_Past(ef);
         return x;
     }
     [WebMethod]
@@ -105,11 +107,11 @@ public class Service : System.Web.Services.WebService
         return ef.Gen_AppID();
     }
     [WebMethod]
-    public int Pass_Reset(StudLoginFields ef)
+    public string Pass_Reset(StudLoginFields ef)
     {
 
         ResetPasswordLogic DB = new ResetPasswordLogic();
-        int txt = DB.PassReset(ef);
+        string txt = DB.PassReset(ef);
         return txt;
     }
 
